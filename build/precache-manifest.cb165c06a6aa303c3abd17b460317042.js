2e7834ce791dedaf01bee0b2d0800812e89e5784
Mykhaylo Gerasika <mgerasika@gmail.com>
2019-12-06 20:23:49 +0200

    1

---
+++ build/precache-manifest.cb165c06a6aa303c3abd17b460317042.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a82891ba148bdef680973d53795310f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbdd2405506f50407b40",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "30062664bd38ef90eb01",
+    "url": "/static/css/main.986bf13c.chunk.css"
+  },
+  {
+    "revision": "cbdd2405506f50407b40",
+    "url": "/static/js/2.25f1baa1.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.25f1baa1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30062664bd38ef90eb01",
+    "url": "/static/js/main.7ed091b6.chunk.js"
+  },
+  {
+    "revision": "a2076e2b6ff597e10196",
+    "url": "/static/js/runtime-main.30cba478.js"
+  }
+]);
